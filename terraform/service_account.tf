df32076fabb5573c7315c08ca551d3100780efa0
naari3 <naari.named@gmail.com>
2023-12-29 23:06:41 +0900

    attach some iam for volume mount

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -2,3 +2,15 @@ resource "google_service_account" "bakatare" {
   account_id   = "bakatare"
   display_name = "bakatare"
 }
+
+resource "google_project_iam_member" "instance_admin" {
+  project = local.project_id
+  role    = "roles/compute.instanceAdmin"
+  member  = google_service_account.bakatare.member
+}
+
+resource "google_project_iam_member" "service_account_user" {
+  project = local.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = google_service_account.bakatare.member
+}
